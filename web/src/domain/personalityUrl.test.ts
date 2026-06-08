df2ab43f5678b1793564f31e7b4ce29a3312e2fa
donovansmith44 <donovan.smith44@gmail.com>
2026-06-08 18:04:37 -0400

    Revert "Merge personality-test foundation (core domain + Firestore data layer + rules)"
    This reverts commit dd3d5d871585db97a562bab766b82b77b97ab90d, reversing
    changes made to 9b71add22e72c3d9270e3c8aa671bd4a234f1fae.

--- web/src/domain/personalityUrl.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest'
-import { personalityUrl } from './personalityUrl'
-
-describe('personalityUrl', () => {
-  it('builds a lowercase 16personalities link', () => {
-    expect(personalityUrl('INTJ')).toBe('https://www.16personalities.com/intj-personality')
-    expect(personalityUrl('esfp')).toBe('https://www.16personalities.com/esfp-personality')
-  })
-})
--- web/src/domain/personalityUrl.ts
+++
@@ -1,3 +0,0 @@
-export function personalityUrl(type: string): string {
-  return `https://www.16personalities.com/${type.toLowerCase()}-personality`
-}
